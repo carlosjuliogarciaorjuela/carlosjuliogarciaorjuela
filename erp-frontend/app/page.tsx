47f2b7da3e1e69b11c8af252cb893d25136c1d9e
carlosjuliogarciaorjuela <cegarcia2346@gmail.com>
2025-02-03 18:18:21 -0500

    add:logokyj erp_frontend

--- erp-frontend/app/page.tsx
+++ erp-frontend/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/logokyj.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -14,13 +14,13 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Soluciones S.A.S{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Un servicio seguro y de calidad.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
