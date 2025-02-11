4c069d5a64894e4774caa81a959982def1cc27e5
carlosjuliogarciaorjuela <cjgarcia655@soy.sena.edu.co>
2025-02-11 00:44:35 -0500

    add: file static

--- erp-frontend/next.config.ts
+++ erp-frontend/next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
